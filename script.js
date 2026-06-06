0a27922b572683d0f413fd9b26c0cb1a4dcb088d
Andrew-Sackey <sackeya24@gmail.com>
2026-06-06 19:00:11 +0000

    last last

--- script.js
+++ script.js
@@ -689,7 +689,7 @@ const MENU_DATA = {
         { name: 'Croque Madame Bagel', desc: 'Sourdough bread, ham, emmental cheese, egg, mayonnaise & béchamel sauce', img: './images/mi-croque-madame.webp' },
         { name: 'Balsamic Avocado Bagel', desc: 'Bagel, avocado, tomatoes, red onions, mayonnaise & black pepper', img: './images/bagel.webp' },
         { name: 'Granola Bowl', desc: 'Granola, fresh berries, honey and yoghurt', img: './images/men13.webp' },
-        { name: 'French Toast', desc: 'French toast with ice cream or maple syrup', img: './images/french-toast.webp' },
+        { name: 'French Toast', desc: 'French toast with ice cream or maple syrup', img: '' },
         { name: 'American Breakfast', desc: '4 pancakes, 3 bacon, scrambled eggs & fresh orange juice', img: './images/ame-breakfast.webp' },
         { name: 'Lebanese Breakfast', desc: 'Lebanese bread, labneh with extra virgin olive oil, green olives, cucumbers, tomatoes, minced meat with eggs & hot black tea', img: './images/mi-lebanese-breakfast.webp' },
         { name: 'English Breakfast', desc: '2 toast, 2 bacon, sausage, hashbrowns, fresh tomatoes, mushrooms, baked beans, eggs & fresh juice', img: './images/men10.webp' },
@@ -731,10 +731,10 @@ const MENU_DATA = {
         { name: 'Meatball Spaghetti', desc: 'Spaghetti, meatballs, tomato sauce, fresh parsley & black pepper', img: './images/men16.webp' },
         { name: 'Jollof Rice & Fish', desc: 'Smoky Ghanaian jollof with grilled fish', img: '' },
         { name: 'Jollof Rice & Chicken', desc: 'Smoky Ghanaian jollof with tender chicken', img: './images/gal6.webp' },
-        { name: 'Jollof Rice & Goat Meat', desc: 'Smoky Ghanaian jollof with slow-cooked goat meat', img: './images/jollof-goat.webp' },
-        { name: 'Fried Rice & Fish', desc: 'Wok-fried seasoned rice with grilled fish', img: './images/mi-fried-rice-fish.webp' },
+        { name: 'Jollof Rice & Goat Meat', desc: 'Smoky Ghanaian jollof with slow-cooked goat meat', img: '' },
+        { name: 'Fried Rice & Fish', desc: 'Wok-fried seasoned rice with grilled fish', img: '' },
         { name: 'Fried Rice & Chicken', desc: 'Wok-fried seasoned rice with chicken', img: './images/men22.webp' },
-        { name: 'Fried Rice & Goat Meat', desc: 'Wok-fried seasoned rice with slow-cooked goat meat', img: './images/fried-rice-goat.webp' },
+        { name: 'Fried Rice & Goat Meat', desc: 'Wok-fried seasoned rice with slow-cooked goat meat', img: '' },
         { name: 'Yam Chips & Goat Meat', desc: 'Crispy yam chips paired with goat meat', img: './images/yam-goat.webp' },
         { name: 'Fish & Chips', desc: 'Golden fried fish fillet with crispy chips', img: './images/gal10.webp' },
       ]
@@ -768,7 +768,7 @@ const MENU_DATA = {
         { name: 'Raspberry Heaven', desc: 'Raspberry, blueberry, apple and mango', img: './images/mi-raspberry-heaven.webp' },
         { name: 'Pineapple Sunset', desc: 'Pineapple, papaya and mango', img: './images/mi-pineapple-sunset.webp' },
         { name: 'Organic Sunshine', desc: 'Pineapple, mango and banana', img: '' },
-        { name: 'Mango & Strawberry', desc: 'Mango, strawberry and orange juice', img: './images/mango-strawberry-fusion.jpeg' },
+        { name: 'Mango & Strawberry', desc: 'Mango, strawberry and orange juice', img: '' },
         { name: 'Green Reviver', desc: 'Kale, lemon juice, orange juice and banana', img: '' },
         { name: 'Healthy Living', desc: 'Avocado, dates, banana and almond milk', img: '' },
       ]
@@ -788,14 +788,14 @@ const MENU_DATA = {
         { name: 'Baileys Freakshake', desc: 'An indulgent tower of cream, chocolate and Baileys', img: '' },
         { name: 'Velvet Dream', desc: 'Strawberry ice cream milkshake, red velvet cake, whipped cream & strawberry syrup', img: '' },
         { name: 'Fudge Deluxe', desc: 'Chocolate milkshake, nutella, oreo pieces, chocolate syrup, brownie waffle pieces, vanilla scoop & mini pancakes', img: './images/men12.webp' },
-        { name: 'Dolcé Swirl', desc: 'Caramel ice cream milkshake, mini pancakes, maple syrup, cinnamon & whipped cream', img: './images/men26.webp' },
+        { name: 'Dolcé Swirl', desc: 'Caramel ice cream milkshake, mini pancakes, maple syrup, cinnamon & whipped cream', img: '' },
       ]
     },
     pancakes: {
       label: 'Pancakes',
       items: [
         { name: 'Cinnamon Pancakes', desc: 'Fluffy pancakes dusted with warm cinnamon', img: '' },
-        { name: 'Biscoff Pancakes', desc: 'Pancakes with biscoff spread and biscuits', img: '' },
+        { name: 'Biscoff Pancakes', desc: 'Pancakes with biscoff spread and biscuits', img: './images/men26.webp' },
         { name: 'Pancake Cone Combo', desc: 'Pancake stack with two scoops of ice cream on a cone', img: './images/men14.webp' },
         { name: 'Pancake with Ice Cream', desc: 'Soft pancake stack with a generous scoop of ice cream', img: './images/pan-ice.webp' },
       ]
